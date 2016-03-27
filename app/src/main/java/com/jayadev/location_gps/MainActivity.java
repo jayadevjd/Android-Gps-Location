93be19f6a7de250b893a522a5a32c1e15e90f69e
Jayadev <mail.jayadev.jd@gmail.com>
2016-03-27 22:01:22 +0530

    changes in activity

--- app/src/main/java/com/jayadev/location_gps/MainActivity.java
+++ app/src/main/java/com/jayadev/location_gps/MainActivity.java
@@ -1,6 +1,5 @@
 package com.jayadev.location_gps;
 
-import android.app.Activity;
 import android.content.Context;
 import android.location.Address;
 import android.location.Geocoder;
@@ -8,11 +7,8 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
